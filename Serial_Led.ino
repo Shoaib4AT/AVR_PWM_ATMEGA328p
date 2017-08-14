e6cb7ebac758e4908d44d7b333a0b94742914e42
Shoaib Ahmed Shaikh <30997997+Shoaib4AT@users.noreply.github.com>
2017-08-14 12:34:26 +0530

    Add files via upload

---
+++ Serial_Led.ino
@@ -0,0 +1,28 @@
+
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+  String  inVal = "";
+
+  while (Serial.available() > 0)
+  {
+    inVal += (char) Serial.read();
+    delay(5);
+  }
+  if (inVal == "ON")
+  {
+    Serial.println("LED ON");
+    digitalWrite(13, HIGH);
+  }
+  else if (inVal == "OFF")
+  {
+    Serial.println("LED OFF");
+    digitalWrite(13, LOW);
+  }
+}
